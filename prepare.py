bb1b63a2106053c58b265d4dd4cbcf07e0f9ccbd
Andrew Konstans <andrew.konstans10@gmail.com>
2023-03-06 13:48:53 -0600

    models

--- prepare.py
+++ prepare.py
@@ -6,6 +6,14 @@ import acquire as a
 import re
 import numpy as np
 import pandas as pd
+from nltk.corpus import stopwords
+from nltk.tokenize.toktok import ToktokTokenizer
+import requests
+import unicodedata
+import nltk
+from textblob import TextBlob, Word
+from nltk.stem import PorterStemmer, SnowballStemmer
+from nltk.stem import WordNetLemmatizer
 
 def get_links(csv_file):
     df = pd.read_csv(csv_file)
@@ -44,3 +52,68 @@ def clean_languages(df):
 def drop_blanks(df):
     df = df[df.readme_contents != '']
     return df
+
+def split(input_string):
+    '''
+    Creates and uses a snowball stemmer for readme data
+    '''
+    splitter = nltk.stem.SnowballStemmer('english')
+    
+    stem = [splitter.stem(word) for word in input_string.split()]
+    split_string = ' '.join(stem)
+    
+    return split_string
+
+def lemmatized(input_string):
+    """
+    Takes an input string and lemmatizes it.
+    Please do not stem and lemmatize the same string.
+    """
+    #Creates the lemmatizer object
+    wnl = nltk.stem.WordNetLemmatizer()
+    
+    #Makes lemmatade
+    lemmas = [wnl.lemmatize(word) for word in input_string.split()]
+    lemmatized_string = ' '.join(lemmas)
+    
+    return lemmatized_string
+
+def remove_stopwords(input_string, extra_words=None, exclude_words=None):
+    """
+    This function takes an input and removes stopwords. You can add or remove words witht the extra_words or exclude_words args.
+    """
+    
+    stopword_list = stopwords.words('english')
+    if extra_words != None:
+        stopword_list.extend(extra_words)
+    if exclude_words != None:
+        if type(extra_words) == str:
+            stopword_list.remove(exclude_words)
+        if type(extra_words) == list:
+            for word in exclude_words:
+                stopword_list.remove(word)
+    words = input_string.split()
+    filtered_words = [w for w in words if w not in stopword_list]
+    string_without_stopwords = ' '.join(filtered_words)
+
+    return string_without_stopwords
+
+
+def basic_clean(input_string):
+    """
+    This function takes in a string and applies basic cleaning to it.
+    """
+    #Changes all characters to their lower case.
+    input_string = input_string.lower()
+    
+    input_string = unicodedata.normalize('NFKD', input_string).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    
+    #Removes special characters
+    input_string = re.sub(r"[^a-z0-9\s]", '', input_string)
+    input_string = re.sub(r'\n', ' ', input_string)
+    input_string = re.sub(r'\s{2,}', ' ', input_string)
+    return input_string
+
+def tokenized(input_string):
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+    return tokenizer.tokenize(input_string, return_str=True)
